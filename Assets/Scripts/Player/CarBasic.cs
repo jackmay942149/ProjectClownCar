f9fb9d77602afb41ec5d216b6107a03fe2548d1d
Jack May <jackmay942149@gmail.com>
2024-09-09 08:55:07 +1000

    feat(car): Added basic car script but its buggy as hell, have to look into force and mass issues

--- Assets/Scripts/Player/CarBasic.cs
+++ Assets/Scripts/Player/CarBasic.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Car : MonoBehaviour
+public class CarBasic : MonoBehaviour
 {
     [Header("PickUp Information")]
     public GameObject[] clownPositions;
@@ -12,19 +12,19 @@ public class Car : MonoBehaviour
     private Rigidbody rb;
     private float forwardInput; // Player input
     private float sidewaysInput; // Player Input
+    [SerializeField] private float maxSpeed;
+    [SerializeField] private float acceleration;
+    [SerializeField] private float turnAccel;
+    [SerializeField] private float carRotationAccel;
 
-    [Header("Updated Car Movement Mechanics")]
-    [SerializeField] private Suspension[] suspensions;
-    [SerializeField] private float accelerationFactor = 1.0f;
-    [SerializeField] private float turningFactor = 30.0f;
- 
     [Header("Objective Information")]
     private ObjectiveManager objectiveManager;
+
+    // Start is called before the first frame update
     void Start(){
         rb = GetComponent<Rigidbody>();
         objectiveManager = GameManager.GetInstance().objectiveManager.GetComponent<ObjectiveManager>();
     }
-
 
     void Update(){
         // Enable Objective Text
@@ -35,30 +35,28 @@ public class Car : MonoBehaviour
 
         // Get input from arrow keys
         forwardInput = Input.GetAxisRaw("Vertical");
-        sidewaysInput = Input.GetAxisRaw("Horizontal");
+        sidewaysInput = Input.GetAxis("Horizontal");
     }
 
     void FixedUpdate(){
+        // Apply forward force
+        Vector3 inputForce = Vector3.zero;
 
-        float forwardMovement = forwardInput * Time.fixedDeltaTime * accelerationFactor * 10000;
-        float sidewaysMovement = sidewaysInput * Time.fixedDeltaTime * turningFactor;
-
-        foreach (Suspension suspension in suspensions){
-            bool inputForward = true;
-            if (forwardInput == 0.0f) {inputForward = false;}
-            suspension.UpdateCurrentForce(forwardMovement, inputForward);
+        if (rb.velocity.magnitude < maxSpeed){
+            inputForce.z += (forwardInput * acceleration);
+            Debug.Log("Adding Velocity");
         }
 
-        foreach (Suspension suspension in suspensions){
-            Vector3 forceToApply = transform.TransformDirection(suspension.currentForce);
-            if (suspension.steeringWheel){forceToApply = Quaternion.Euler(0, turningFactor * sidewaysInput, 0) * forceToApply;}
-            if (suspension.isTouchingGround){
-                rb.AddForceAtPosition(forceToApply, suspension.transform.position);
-            }
-            
-        }     
-    }
+        inputForce.x += sidewaysInput * turnAccel * rb.velocity.magnitude;
 
+        // Turn the car
+        Vector3 turnVec = Vector3.zero;
+        turnVec.y = sidewaysInput * carRotationAccel * rb.velocity.magnitude;
+        transform.Rotate(turnVec);
+
+        rb.AddRelativeForce(inputForce);
+    }
+    
     public void PickupClown(GameObject clown, int positionInCar)
     {
         clown.transform.position =  new Vector3 (0.0f, 0.0f, 0.0f);
