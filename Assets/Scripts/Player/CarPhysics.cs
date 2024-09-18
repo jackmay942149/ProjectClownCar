034bb30852262348ff3792f1323d07d05bcce9d4
Jack May <92079641+jackmay942149@users.noreply.github.com>
2024-09-18 23:05:38 +1000

    feat(car): Made Improvements on car steering and wheel spinning

---
+++ Assets/Scripts/Player/CarPhysics.cs
@@ -0,0 +1,79 @@
+using System.Collections;
+using System.Collections.Generic;
+using Unity.PlasticSCM.Editor.WebApi;
+using UnityEngine;
+using UnityEngine.Rendering;
+using UnityEngine.XR;
+
+
+[System.Serializable]
+struct Wheel {
+    public GameObject wheel;
+    public bool isSteering;
+    public bool isDriving;
+}
+
+public class CarPhysics : MonoBehaviour
+{
+    // Object References
+    [SerializeField] private List<Wheel> wheels;
+    [SerializeField] private GameObject steeringWheel;
+
+    // Component References
+    private Rigidbody rb;
+
+    // Input Variables
+    private float adInput;
+    private float wsInput;
+
+    // Adjustable Values
+    [Header("Wheel Settings")]
+    public float wheelRotationSpeed;
+    public float maxWheelTurnAngleDegrees;
+
+    [Header("Steering Wheel Settings")]
+    public float maxSteeringWheelTurnAngleDegrees;
+
+    
+
+    // Start is called before the first frame update
+    void Start(){
+        rb = GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void Update(){
+        adInput = Input.GetAxis("Horizontal");
+        wsInput = Input.GetAxisRaw("Vertical");
+
+        // Debug.Log(adInput.ToString() + wsInput.ToString());
+    }
+
+    void FixedUpdate(){
+        // Adjust Wheels Rotation ... need to calculate the tilt (moving forward and backward) as well as yaw (steering left and right)
+        foreach (Wheel w in wheels){
+            if (w.isSteering) {
+                w.wheel.transform.SetLocalPositionAndRotation(w.wheel.transform.localPosition, 
+                    Quaternion.Euler(w.wheel.transform.localRotation.eulerAngles.x + wsInput * wheelRotationSpeed, maxWheelTurnAngleDegrees * adInput, 0));
+            }
+            else {
+                w.wheel.transform.SetLocalPositionAndRotation(w.wheel.transform.localPosition,
+                    Quaternion.Euler(w.wheel.transform.localRotation.eulerAngles.x + wsInput * wheelRotationSpeed, 0, 0));
+            }
+        }
+
+        /* Adjust Steering Wheel Rotation
+        if (steeringWheel) {
+            steeringWheel.transform.SetLocalPositionAndRotation(steeringWheel.transform.position,
+                Quaternion.Euler(0, steeringWheel.transform.localRotation.eulerAngles.y + adInput * maxSteeringWheelTurnAngleDegrees, 0));
+        }
+        */
+
+        // Adjust Wheels Movement .. need to calculate height (raycast to ground)
+
+        // Adjust Car Body Rotation .. need to calculate the tilt (average front wheel height vs back heel height), yaw (wheel speed and direction), roll all based of the wheel locations (average left wheel height vs right wheel height)
+
+        // Adjust Car Position ... adjust forward movement based of wheel speed
+    }
+    
+}
