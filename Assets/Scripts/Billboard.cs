0bdf904f6b6618912d14d7ec2797a31a3d324b5a
Jack May <jackmay942149@gmail.com>
2024-10-09 10:02:19 +1000

    feat(Billboard): Removed old billboard script and tidied new one

--- Assets/Scripts/Billboard.cs
+++ Assets/Scripts/Billboard.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class CarBillBoard : MonoBehaviour
+public class Billboard : MonoBehaviour
 {
     public Material billboardMat;
     void Start(){
